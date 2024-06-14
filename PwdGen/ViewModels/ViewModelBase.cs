a542e30d70c17d3d696691f7011762c178f8c48c
tsctsc6@126.com <tsctsc6@126.com>
2024-06-14 19:06:39 +0800

    添加项目文件。

---
+++ PwdGen/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PwdGen.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ PwdGen/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PwdGen.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
