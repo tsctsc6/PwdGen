36a496b51f03622e25ef11a3d1fae6ee43a18696
tsctsc6@126.com <tsctsc6@126.com>
2024-06-16 13:15:45 +0800

    Add ParentViewModel in ViewModelBase class

--- PwdGen/ViewModels/ViewModelBase.cs
+++ PwdGen/ViewModels/ViewModelBase.cs
@@ -4,4 +4,14 @@ namespace PwdGen.ViewModels;
 
 public class ViewModelBase : ObservableObject
 {
+    public ViewModelBase? ParentViewModel { get; private set; } = null;
+
+    public ViewModelBase()
+    {
+    }
+
+    public ViewModelBase(ViewModelBase? parentViewModel)
+    {
+        ParentViewModel = parentViewModel;
+    }
 }
